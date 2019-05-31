23838b9e79f7572d617de8ed8c46024466deb9f0
skolix15 <50676066+skolix15@users.noreply.github.com>
2019-05-31 12:25:44 +0300

    testing

--- SoftEng/src/Main.java
+++ SoftEng/src/Main.java
@@ -1,5 +1,7 @@
 import java.sql.SQLException;
 
+import org.jfree.chart.JFreeChart;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -21,7 +23,6 @@ public class Main {
 */
 
 	   new GlobalHomeFrame(conn);
-	   
 	   
 	}
 	
